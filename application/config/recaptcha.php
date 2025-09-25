25db2b0a176e24f5f284dbed70f3b0d5bd4f3a95
Ariell Cannal <ariell@cannal.com.br>
2025-09-25 19:25:30 -0300

    CI4

--- application/config/recaptcha.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (! defined('BASEPATH'))
-    exit('No direct script access allowed');
-
-$config['recaptcha_key'] = getenv('recaptcha_key');
-?>
--- application/views/alunos/header_aluno.php
+++
@@ -1,5 +0,0 @@
-<?php if($this->previous_task == 'save'):?>
-<script>
-	alertify.alert('Aluno','Cadastro salvo com sucesso');
-</script>
-<?php endif;?>
